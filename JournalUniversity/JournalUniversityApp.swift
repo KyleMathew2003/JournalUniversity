82fbe3c8f1f4b1a4b29b581f1352e22d6be8e986
KyleMathew2003 <kyletmathew@gmail.com>
2022-08-07 14:40:24 -0400

    Initial Commit

---
+++ JournalUniversity/JournalUniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalUniversityApp.swift
+//  JournalUniversity
+//
+//  Created by Kyle Mathew on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct JournalUniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
